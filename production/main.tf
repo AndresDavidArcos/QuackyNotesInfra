f290ed238780510823ad2bde1b0f8d2656fde6cf
Andres <andres.camargo@correounivalle.edu.co>
2023-04-05 17:46:05 -0500

    production files

---
+++ production/main.tf
@@ -0,0 +1,91 @@
+variable "staging_public_key" {
+  description = "Staging environment public key value"
+  type        = string
+}
+
+variable "base_ami_id" {
+  description = "Base AMI ID"
+  type        = string
+}
+
+variable "access_key" {
+  type        = string
+}
+
+variable "secret_key" {
+  type        = string
+}
+
+terraform {
+  backend "remote" {
+    organization = "terrastates"
+
+    workspaces {
+      name = "QuackyNotesInfra"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.48"
+    }
+
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+
+  required_version = ">= 0.15.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+
+
+resource "aws_key_pair" "production_key" {
+  key_name   = "staging-key"
+  public_key = var.production_public_key
+
+  tags = {
+    "Name" = "production_public_key"
+  }
+}
+
+resource "aws_security_group" "production_rules" {
+  name        = "production_rules"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  description = "HTTP access"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
---
+++ production/production.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "production_quackynoteapp" {
+
+  ami                    = var.base_ami_id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.production_rules.id]
+  key_name               = aws_key_pair.production_key.key_name
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get -y update
+    sudo apt-get -y install curl
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 7EA0A9C3F273FCD8
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+    apt-cache policy docker-ce
+    sudo apt install -y docker-ce
+  EOF
+  tags = {
+    "Name" = "production_quackynoteapp"
+  }
+}
+
+output "production_dns" {
+  value = aws_instance.production_quackynoteapp.public_dns
+}
